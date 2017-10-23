519c107b814295870fb75d29dd71f9317fda1215
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-10-23 23:41:52 +0100

    ugly hack to write files

--- my_utils/datawriter.py
+++ my_utils/datawriter.py
@@ -9,11 +9,13 @@ def print_clustered(filename: str, tweets: List[Tweet]):
         submwriter = csv.writer(reddit_posts_csv, delimiter=',',
                                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for tweet in tweets:
+            # TODO: Fix this ugly hack of the new lines
+            tweet_text = tweet.tweet_text.replace('\n', ' ')
             submwriter.writerow((tweet.cluster_id,
                                 tweet.cluster_name_entity,
                                 tweet.tweet_id,
                                 tweet.timestamp_ms,
                                 tweet.user_id,
                                 tweet.tweet_tokens,
-                                tweet.tweet_text))
+                                tweet_text))
 
