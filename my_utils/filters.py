1e9418b512fd018db70b69f822a681320e8a4399
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-10-24 07:33:55 +0100

    Improved filter algorithm.

--- my_utils/filters.py
+++ my_utils/filters.py
@@ -1,20 +1,24 @@
+from typing import Dict, List
+
 import numpy as np
 
 
-def threeshold_filter(clusters: dict, cluster_counts: dict, timestamps: np.array, cluster_filter_threshold: int):
+def threeshold_filter(clusters: Dict[int, str], cluster_counts: Dict[int, int], timestamps: np.array,
+                      cluster_filter_threshold: int):
     # filter out small clusters & centroid calculation
-    filtered_clusters = set()
-    relevant_cluster_centroids = []
+    filtered_clusters: set = set()
+    relevant_cluster_centroids: List[List[int]] = []
     cluster_entities = {}
 
     for c_id in clusters:
         if cluster_counts[c_id] > cluster_filter_threshold:
-            timestamp_centroid = int(np.mean(timestamps[timestamps[:, 0] == c_id][:, 1]))
+            named_entities = set(clusters[c_id].split(' '))
+            if '' in named_entities:
+                named_entities.remove('')
 
+            timestamp_centroid = int(np.mean(timestamps[timestamps[:, 0] == c_id][:, 1]))
             filtered_clusters.add(c_id)
-            cluster_entities[c_id] = set(clusters[c_id].split(' '))
-            if '' in cluster_entities[c_id]:
-                cluster_entities[c_id].remove('')
+            cluster_entities[c_id] = named_entities
             relevant_cluster_centroids.append([c_id, timestamp_centroid])
 
     return filtered_clusters, relevant_cluster_centroids, cluster_entities
