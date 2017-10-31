781e50ee4110df3d5e82df68d87efc7439c49a4a
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-10-31 22:32:21 +0000

    Traffic detection

---
+++ traffic.py
@@ -0,0 +1,26 @@
+from typing import Set
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+from my_utils.datareader import read_clustered
+from entities.tweet import Tweet
+
+
+
+# get the traffic words:
+with open('traffic_words.txt') as f:
+    content = f.readlines()
+traffic_words: Set[str] = set([x.strip() for x in content])
+
+# Read only the tweets!
+_,_,_,t = read_clustered('data/1day/clusters.sortedby.clusterid.csv', True)
+
+# retokenize tweets:
+a = 0
+for tweet in t:
+    tweet : Tweet = tweet
+    words = word_tokenize(tweet.tweet_text)
+    for w in words:
+        if w in traffic_words:
+            a = a + 1
+
+print(a)
