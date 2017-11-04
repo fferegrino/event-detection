f80de4f121a7154a114f39340f70c20c56fee154
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-11-04 08:48:26 +0000

    eval.py to evaluate all results in one go!

--- Makefile
+++ Makefile
@@ -4,6 +4,10 @@ run:
 	python detector.py 50 3600 "data/1day/clusters.sortedby.clusterid.csv" -o results/1day-3600-50.csv
 	python detector.py 50 7200 "data/1day/clusters.sortedby.clusterid.csv" -o results/1day-7200-50.csv
 	python detector.py 50 14400 "data/1day/clusters.sortedby.clusterid.csv" -o results/1day-14400-50.csv
+	python detector.py 10 60 "data/1day/clusters.sortedby.clusterid.csv" -o results/1day-60-10.csv
+	python detector.py 10 3600 "data/1day/clusters.sortedby.clusterid.csv" -o results/1day-3600-10.csv
+	python detector.py 10 7200 "data/1day/clusters.sortedby.clusterid.csv" -o results/1day-7200-10.csv
+	python detector.py 10 14400 "data/1day/clusters.sortedby.clusterid.csv" -o results/1day-14400-10.csv
 	python detector.py 5 60 "data/1day/clusters.sortedby.clusterid.csv" -o results/1day-60-5.csv
 	python detector.py 5 3600 "data/1day/clusters.sortedby.clusterid.csv" -o results/1day-3600-5.csv
 	python detector.py 5 7200 "data/1day/clusters.sortedby.clusterid.csv" -o results/1day-7200-5.csv
@@ -12,10 +16,16 @@ run:
 	python detector.py 50 3600 "data/7days/clusters.sortedby.clusterid.csv" -o results/7days-3600-50.csv
 	python detector.py 50 7200 "data/7days/clusters.sortedby.clusterid.csv" -o results/7days-7200-50.csv
 	python detector.py 50 14400 "data/7days/clusters.sortedby.clusterid.csv" -o results/7days-14400-50.csv
+	python detector.py 10 60 "data/7days/clusters.sortedby.clusterid.csv" -o results/7days-60-10.csv
+	python detector.py 10 3600 "data/7days/clusters.sortedby.clusterid.csv" -o results/7days-3600-10.csv
+	python detector.py 10 7200 "data/7days/clusters.sortedby.clusterid.csv" -o results/7days-7200-10.csv
+	python detector.py 10 14400 "data/7days/clusters.sortedby.clusterid.csv" -o results/7days-14400-10.csv
 	python detector.py 5 60 "data/7days/clusters.sortedby.clusterid.csv" -o results/7days-60-5.csv
 	python detector.py 5 3600 "data/7days/clusters.sortedby.clusterid.csv" -o results/7days-3600-5.csv
 	python detector.py 5 7200 "data/7days/clusters.sortedby.clusterid.csv" -o results/7days-7200-5.csv
 	python detector.py 5 14400 "data/7days/clusters.sortedby.clusterid.csv" -o results/7days-14400-5.csv
+evaluate_all:
+	python eval.py results/
 evaluate:
 	python eval.py results/1day-60-50.csv> results/1day-60-50.txt
 	python eval.py results/1day-3600-50.csv> results/1day-3600-50.txt
@@ -29,8 +39,8 @@ evaluate:
 	python eval.py results/7days-3600-50.csv> results/7days-3600-50.txt
 	python eval.py results/7days-7200-50.csv > results/7days-7200-50.txt
 	python eval.py results/7days-14400-50.csv > results/7days-14400-50.txt
-	python eval.py results/7days-60-5.csv> results/7days-60-5.txt
-	python eval.py results/7days-3600-5.csv> results/7days-3600-5.txt
+	python eval.py results/7days-60-5.csv > results/7days-60-5.txt
+	python eval.py results/7days-3600-5.csv > results/7days-3600-5.txt
 	python eval.py results/7days-7200-5.csv > results/7days-7200-5.txt
 	python eval.py results/7days-14400-5.csv > results/7days-14400-5.txt
 evaluate_professor:
--- eval.py
+++ eval.py
@@ -11,6 +11,9 @@ expected, isn't it?
 
 import sys
 import csv
+import os
+from os import listdir
+from os.path import isdir, isfile, join
 
 MIN_PCT_COVERAGE_THRESHOLD = 5
 MIN_TWEET_COVERAGE_THRESHOLD = 15
@@ -26,6 +29,30 @@ def main():
         return
 
     eval_file = sys.argv[1]
+
+    files_to_evaluate = []
+
+    if isdir(eval_file):
+        for f in listdir(eval_file):
+            ff = join(eval_file, f)
+            if isfile(ff) and ff.endswith(".csv"):
+                g1 = f.index('-')
+                g2 = f.index('-', g1+1)
+                d = f.index('.', g2+1)
+                data = f[0:g1]
+                time = f[g1+1:g2]
+                t = f[g2+1:d]
+                files_to_evaluate.append({
+                    "file": ff,
+                    "day": data,
+                    "desc": "T: " +  t + "\tW: " +  time
+                })
+    else:
+        files_to_evaluate.append({
+            "file": eval_file,
+            "day": "?",
+            "desc": "Original file" + eval_file
+        })
 
     events = {}
     descriptions = {}
@@ -57,137 +84,153 @@ def main():
             cat_count[cat] = 0
         cat_count[cat] = cat_count[cat] + 1
 
-    table = []
 
-    candidates = {}
-    # read as csv instead of raw file
-    with open(eval_file, 'r', encoding='utf-8') as reddit_posts_csv:
-        reader = csv.reader(reddit_posts_csv, delimiter=',',
-                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        line = 0
-        for row in reader:
-            line += 1
-            parts = row
-            if len(parts) < 2:
-                print("Line", line, "malformed.")
-                print("Expected:\t[cluster_id],[named_entity],[tweet_id]")
-                print("Or:\t\t[cluster_id],[tweet_id]")
-                print("\nGot:\t\t", row)
-                return
+    with open("overall-results.csv", 'w', encoding='utf8', newline='') as file:
+        csv_writer = csv.writer(file, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for evaluation in files_to_evaluate:
 
-            candidate = str(parts[0].strip())
-            tweet = str(parts[1].strip())
-            if len(parts) > 2:
-                tweet = str(parts[2].strip())
 
-            if candidate not in candidates:
-                candidates[candidate] = set()
-            candidates[candidate].add(tweet)
+            table = []
 
-        valid_count = 0
-        above_threshold = []
-        found_above_threshold = []
-        ret_cat_count = {}
-        cat_ent_count = {}
-        cat_event_count = {}
-        relevant_tweets = []
-        possible_tweets = []
-        total_tweets = 0
+            candidates = {}
 
-        rel_found_event = {}
+            # read as csv instead of raw file
+            with open(evaluation["file"], 'r', encoding='utf-8') as reddit_posts_csv:
+                reader = csv.reader(reddit_posts_csv, delimiter=',',
+                                    quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                line = 0
+                for row in reader:
+                    line += 1
+                    parts = row
+                    if len(parts) < 2:
+                        print("Line", line, "malformed, file " + evaluation["file"])
+                        print("Expected:\t[cluster_id],[named_entity],[tweet_id]")
+                        print("Or:\t\t[cluster_id],[tweet_id]")
+                        print("\nGot:\t\t", row)
+                        return
 
-        print("CLUSTER_ID\tTWEET COVERAGE\tEVENT DESCRIPTION FROM CROWDSOURCED EVALUATION")
-        for candidate in sorted(candidates):
-            valid_count += 1
-            # print candidate, ",",
+                    candidate = str(parts[0].strip())
+                    tweet = str(parts[1].strip())
+                    if len(parts) > 2:
+                        tweet = str(parts[2].strip())
 
-            relevant = {}
-            for tweet in candidates[candidate]:
-                if tweet in events:
-                    if events[tweet] not in relevant:
-                        relevant[events[tweet]] = 0
-                        rel_found_event[events[tweet]] = []
-                    rel_found_event[events[tweet]].append(tweet)
-                    relevant[events[tweet]] += 1
-                    relevant_tweets.append(tweet)
-            total_tweets += len(candidates[candidate])
+                    if candidate not in candidates:
+                        candidates[candidate] = set()
+                    candidates[candidate].add(tweet)
 
-        for candidate in sorted(candidates):
-            relevant = {}
-            rt = []
-            for tweet in candidates[candidate]:
-                if tweet in events:
-                    if events[tweet] not in relevant:
-                        relevant[events[tweet]] = 0
-                    relevant[events[tweet]] += 1
-                    rt.append(tweet)
+                valid_count = 0
+                above_threshold = []
+                found_above_threshold = []
+                ret_cat_count = {}
+                cat_ent_count = {}
+                cat_event_count = {}
+                relevant_tweets = []
+                possible_tweets = []
+                total_tweets = 0
 
-            if len(relevant) >= 0:
-                # print "Event ID:", candidate, "\tContains", str(len(candidates[candidate]))
+                rel_found_event = {}
 
-                passed = []
-                tr = 0
-                for e in relevant:
-                    e = str(e)
+                #print("CLUSTER_ID\tTWEET COVERAGE\tEVENT DESCRIPTION FROM CROWDSOURCED EVALUATION")
+                for candidate in sorted(candidates):
+                    valid_count += 1
+                    # print candidate, ",",
 
-                    if relevant[e] > 5 and (int(100.0 / len(candidates[candidate]) * relevant[e]) >= MIN_PCT_COVERAGE_THRESHOLD or relevant[e] >= MIN_TWEET_COVERAGE_THRESHOLD):
-                        if category[e] not in ret_cat_count:
-                            ret_cat_count[category[e]] = 0
-                            cat_ent_count[category[e]] = []
-                            cat_event_count[category[e]] = 0
+                    relevant = {}
+                    for tweet in candidates[candidate]:
+                        if tweet in events:
+                            if events[tweet] not in relevant:
+                                relevant[events[tweet]] = 0
+                                rel_found_event[events[tweet]] = []
+                            rel_found_event[events[tweet]].append(tweet)
+                            relevant[events[tweet]] += 1
+                            relevant_tweets.append(tweet)
+                    total_tweets += len(candidates[candidate])
 
-                        possible_tweets += tweets_per_event[e]
+                for candidate in sorted(candidates):
+                    relevant = {}
+                    rt = []
+                    for tweet in candidates[candidate]:
+                        if tweet in events:
+                            if events[tweet] not in relevant:
+                                relevant[events[tweet]] = 0
+                            relevant[events[tweet]] += 1
+                            rt.append(tweet)
 
-                        if e not in found_above_threshold:
-                            ret_cat_count[category[e]] = ret_cat_count[category[e]] + 1
-                        cat_event_count[category[e]] += 1
+                    if len(relevant) >= 0:
+                        # print "Event ID:", candidate, "\tContains", str(len(candidates[candidate]))
 
-                        above_threshold.append(candidate)
-                        found_above_threshold.append(e)
+                        passed = []
+                        tr = 0
+                        for e in relevant:
+                            e = str(e)
 
-                        if category[e] not in category_counts:
-                            category_counts[category[e]] = 0
-                        category_counts[category[e]] += 1
-                        total_items_count += 1
+                            if relevant[e] > 5 and (int(100.0 / len(candidates[candidate]) * relevant[e]) >= MIN_PCT_COVERAGE_THRESHOLD or relevant[e] >= MIN_TWEET_COVERAGE_THRESHOLD):
+                                if category[e] not in ret_cat_count:
+                                    ret_cat_count[category[e]] = 0
+                                    cat_ent_count[category[e]] = []
+                                    cat_event_count[category[e]] = 0
 
-                        print(candidate, "\t\t", str(relevant[e]) + "/" + str(len(set(tweets_per_event[e]))) + " (" + str(int(100.0 / len(candidates[candidate]) * relevant[e])) + "%)\t", descriptions[e][:120])
-                    # else:
-                        # print "\t\t", str(relevant[e]) + "/" + str(len(set(tweets_per_event[e]))) + "(" + str(int(100.0 / len(candidates[candidate]) * relevant[e])) + "%)\t", descriptions[e][:120]
+                                possible_tweets += tweets_per_event[e]
 
-                    passed.append(e)
-                    tr += relevant[e]
+                                if e not in found_above_threshold:
+                                    ret_cat_count[category[e]] = ret_cat_count[category[e]] + 1
+                                cat_event_count[category[e]] += 1
 
-                # print ""
+                                above_threshold.append(candidate)
+                                found_above_threshold.append(e)
 
-        if valid_count > 0 and len(set(above_threshold)) > 0 and len(set(found_above_threshold)) > 0:
-            event_recall = float(
-                len(set(found_above_threshold))) / len(descriptions)
-            event_precision = float(len(set(above_threshold))) / valid_count
-            f_measure = 2 * ((event_precision * event_recall) / (event_precision + event_recall))
+                                if category[e] not in category_counts:
+                                    category_counts[category[e]] = 0
+                                category_counts[category[e]] += 1
+                                total_items_count += 1
 
-            print()
-            print("-------   EVENTS   -------\t")
-            print()
-            print("EVENTS / CLUSTER STATISTICS:")
-            print(" - Of", len(descriptions), "events,", len(set(found_above_threshold)), "were detected.")
-            print(" - Of", valid_count, "clusters,", len(set(above_threshold)), "could be matched back to an event.")
-            print("")
-            print("Event Recall:\t\t%1.3f" % (event_recall))
-            print("Cluster Precision:\t%1.3f" % (event_precision))
-            print("Overall F-Measure:\t%1.3f" % (f_measure))
+                                #print(candidate, "\t\t", str(relevant[e]) + "/" + str(len(set(tweets_per_event[e]))) + " (" + str(int(100.0 / len(candidates[candidate]) * relevant[e])) + "%)\t", descriptions[e][:120])
+                            # else:
+                                # print "\t\t", str(relevant[e]) + "/" + str(len(set(tweets_per_event[e]))) + "(" + str(int(100.0 / len(candidates[candidate]) * relevant[e])) + "%)\t", descriptions[e][:120]
 
-            print()
-            print()
-            print("----- CATEGORIES  -----")
-            print()
-            print("%-30s    RECALL" % ("CATEGORY NAME"))
-            for cat in sorted(ret_cat_count): # order dictionary for easier comparison
-                print("%-30s  %3d/%d\t(%1.3f)" % (cat, ret_cat_count[cat], cat_count[cat], ret_cat_count[cat] / float(cat_count[cat])))
-            print("")
+                            passed.append(e)
+                            tr += relevant[e]
 
-            print()
-        else:
-            print("Didn't find any event clusters that could be matched to an event.")
+                        # print ""
+
+                if valid_count > 0 and len(set(above_threshold)) > 0 and len(set(found_above_threshold)) > 0:
+                    event_recall = float(
+                        len(set(found_above_threshold))) / len(descriptions)
+                    event_precision = float(len(set(above_threshold))) / valid_count
+                    f_measure = 2 * ((event_precision * event_recall) / (event_precision + event_recall))
+
+
+                    csv_writer.writerow((evaluation["day"],
+                                         evaluation["desc"],
+                                         str(len(descriptions)) + " / " + str(len(set(found_above_threshold))),
+                                         str(valid_count) + " / " + str(len(set(above_threshold))),
+                                         "%1.3f" % (event_recall),
+                                         "%1.3f" % (event_precision),
+                                         "%1.3f" % (f_measure)))
+
+                    print("-------   EVENTS   -------\t")
+                    print()
+                    print("EVENTS / CLUSTER STATISTICS:")
+                    print(" - Of", len(descriptions), "events,", len(set(found_above_threshold)), "were detected.")
+                    print(" - Of", valid_count, "clusters,", len(set(above_threshold)), "could be matched back to an event.")
+                    print("")
+                    print("Event Recall:\t\t%1.3f" % (event_recall))
+                    print("Cluster Precision:\t%1.3f" % (event_precision))
+                    print("Overall F-Measure:\t%1.3f" % (f_measure))
+
+                    print()
+                    print()
+                    print("----- CATEGORIES  -----")
+                    print()
+                    print("%-30s    RECALL" % ("CATEGORY NAME"))
+                    for cat in sorted(ret_cat_count): # order dictionary for easier comparison
+                        print("%-30s  %3d/%d\t(%1.3f)" % (cat, ret_cat_count[cat], cat_count[cat], ret_cat_count[cat] / float(cat_count[cat])))
+                    print("")
+
+                    print()
+                else:
+                    print("Didn't find any event clusters that could be matched to an event.")
 
 
 if __name__ == "__main__":
